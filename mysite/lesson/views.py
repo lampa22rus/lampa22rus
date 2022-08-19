6e30f8258db83dc1005acb72d99ff6ba9e698496
lampa22rus <11857@bk.ru>
2022-08-20 01:19:55 +0700

    test
    test

---
+++ mysite/lesson/views.py
@@ -0,0 +1,8 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    print(request)
+    return HttpResponse('Hellow world')
